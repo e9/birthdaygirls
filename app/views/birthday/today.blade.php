2f852c4a4a299e00893bd60a1e9117f95c72340b
Shotaro Osaki <t05181so@gmail.com>
2014-02-08 03:19:31 +0900

    本番のPHPが古い。

--- app/views/birthday/today.blade.php
+++ app/views/birthday/today.blade.php
@@ -5,13 +5,14 @@
 	<h3><a href="girl/{{ $girl->name }}">ハッピーバースデー！{{ $girl->name}}さん<br>{{ $year - $girl->year }}歳の誕生日、おめでとうございます。</a></h3>
 
 	<div class="grid_8 alpha">
-		@if ($movie = $girl->movies()[0])
+		<?php $movies = $girl->movies(); ?>
+		@if ($movie = $movies[0])
 		<p><script src="http://static.fc2.com/video/js/outerplayer.min.js" url="{{ $movie->url }}" w="620" h="380" charset="UTF-8"></script></p>
 		@endif
-		@if ($movie = $girl->movies()[1])
+		@if ($movie = $movies[1])
 		<div class="grid_4 alpha"><script src="http://static.fc2.com/video/js/outerplayer.min.js" url="{{ $movie->url }}" w="300" h="200" charset="UTF-8"></script></div>
 		@endif
-		@if ($movie = $girl->movies()[2])
+		@if ($movie = $movies[2])
 		<div class="grid_4 alpha"><script src="http://static.fc2.com/video/js/outerplayer.min.js" url="{{ $movie->url }}" w="300" h="200" charset="UTF-8"></script></div>
 		@endif
 	</div>
