8f28c20e8f44f8748a073ecb3c03595b810fa771
Shotaro Osaki <t05181so@gmail.com>
2014-02-08 03:16:53 +0900

    動画表示。

--- app/views/birthday/today.blade.php
+++ app/views/birthday/today.blade.php
@@ -5,9 +5,15 @@
 	<h3><a href="girl/{{ $girl->name }}">ハッピーバースデー！{{ $girl->name}}さん<br>{{ $year - $girl->year }}歳の誕生日、おめでとうございます。</a></h3>
 
 	<div class="grid_8 alpha">
-		<p><img data-src="holder.js/620x400"></p>
-		<div class="grid_4 alpha"><img data-src="holder.js/300x200"></div>
-		<div class="grid_4 omega"><img data-src="holder.js/300x200"></div>
+		@if ($movie = $girl->movies()[0])
+		<p><script src="http://static.fc2.com/video/js/outerplayer.min.js" url="{{ $movie->url }}" w="620" h="380" charset="UTF-8"></script></p>
+		@endif
+		@if ($movie = $girl->movies()[1])
+		<div class="grid_4 alpha"><script src="http://static.fc2.com/video/js/outerplayer.min.js" url="{{ $movie->url }}" w="300" h="200" charset="UTF-8"></script></div>
+		@endif
+		@if ($movie = $girl->movies()[2])
+		<div class="grid_4 alpha"><script src="http://static.fc2.com/video/js/outerplayer.min.js" url="{{ $movie->url }}" w="300" h="200" charset="UTF-8"></script></div>
+		@endif
 	</div>
 
 	<div class="grid_2 omega">
--- app/views/girl/show.blade.php
+++ app/views/girl/show.blade.php
@@ -1,15 +1,13 @@
-<h2 class="grid_10 prefix_1 suffix_1">{{ $girl->name }}</h2>
+<h2 class="grid_10 prefix_1 suffix_1">{{ $girl->month }}月{{ $girl->day }}日生まれの{{ $girl->name }}さん</h2>
 
 <div class="grid_10 prefix_1 suffix_1">
 	<div class="grid_8 alpha">
-		<p><img data-src="holder.js/620x400"></p>
-		<p><img data-src="holder.js/620x400"></p>
-		<p class="banner"><img data-src="holder.js/600x80"></p>
-		<p><img data-src="holder.js/620x400"></p>
-		<p><img data-src="holder.js/620x400"></p>
-		<p class="banner"><img data-src="holder.js/600x80"></p>
-		<p><img data-src="holder.js/620x400"></p>
-		<p><img data-src="holder.js/620x400"></p>
+		@foreach ($girl->movies() as $i => $movie)
+			<p><script src="http://static.fc2.com/video/js/outerplayer.min.js" url="{{ $movie->url }}" w="620" h="380" charset="UTF-8"></script></p>
+			@if (($i % 2) == 1)
+				<p class="banner"><img data-src="holder.js/600x80"></p>
+			@endif
+		@endforeach
 	</div>
 
 	<div class="grid_2 omega">
