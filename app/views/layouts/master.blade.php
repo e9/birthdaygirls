5874e100713cfbcc55e30c03cb8c5ee11fcd21ac
Shotaro Osaki <t05181so@gmail.com>
2014-02-12 16:51:06 +0900

    アナリティクス。

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -10,6 +10,16 @@
 <script src="//imsky.github.io/holder/holder.js"></script>
 <title>バースデーガールズ</title>
 <meta name="description" content="今日が誕生日のAV女優を毎日、紹介">
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-47921751-1', 'birthdaygirls.info');
+  ga('send', 'pageview');
+
+</script>
 </head>
 <body>
 <h1><a href="/">バースデーガールズ <small>今日が誕生日のAV女優を毎日、紹介</small></a></h1>
