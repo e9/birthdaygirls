bb303e385fc6bdfde045986d0d2ff8924d22f8df
Shotaro Osaki <t05181so@gmail.com>
2014-02-27 03:32:35 +0900

    fc2

--- app/models/FC2Movie.php
+++ app/models/FC2Movie.php
@@ -11,12 +11,12 @@ class FC2Movie {
 	{
 		return Cache::remember("fc2.{$name}", App::environment('production') ? 60*25 : 0, function()use($name)
 		{
-			$html = HtmlDomParser::file_get_html("http://video.fc2.com/a/movie_search.php?ordertype=2&perpage=3&keyword=".urlencode($name));
+			$html = HtmlDomParser::file_get_html("http://video.fc2.com/a/movie_search.php?ordertype=2&perpage=8&keyword=".urlencode($name));
 
 			$res = array_map(function($a){
 				preg_match('/video([0-9]+)-thumbnail/', $a->find('img', 0)->src, $matches);
 				return new FC2Movie($a->href, $a->title, $matches[1]);
-			}, $html->find('.video_list_renew .video_thumb_small a'));
+			}, $html->find('#video_list_1column .clsThumbToAlbum a'));
 
 			$html->clear();
 
