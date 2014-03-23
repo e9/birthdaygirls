f469e1cce01ff93659b3d642ded92725a2932b2c
Shotaro Osaki <t05181so@gmail.com>
2014-03-23 17:42:33 +0900

    dmm

--- app/views/dmm/today.blade.php
+++ app/views/dmm/today.blade.php
@@ -12,14 +12,14 @@
 	@foreach($girls as $girl)
 		<div class="section">
 			<h3 class="bg-white"><a href="/dmm/girl/{{ $girl->name }}">ハッピーバースデー！{{ $girl->name}}さん<br>{{ $year - $girl->year }}歳の誕生日、おめでとうございます。</a></h3>
-			<div class="content"><img data-src="holder.js/740x448/text:thumbnail"></div>
-			<div class="content girllink">
-			<a href="/dmm/girl/{{ $girl->name }}" class="bg-white place-right girllink">もっと見る</a>
-			</div>
 
 			@if ($thumbnail = array_get($girl->dmms(), 0))
 			@include('dmm.thumbnail', array('thumbnail' => $thumbnail))
 			@endif
+
+			<div class="content girllink">
+			<a href="/dmm/girl/{{ $girl->name }}" class="bg-white place-right girllink">もっと見る</a>
+			</div>
 
     		@include('girl.banner')
 		</div>
